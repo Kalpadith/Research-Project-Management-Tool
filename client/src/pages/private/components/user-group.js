38d5063727b8c17fe1b0e2935754d211098eb634
TharushiJayawardena <tharushijayawardena03@gmail.com>
2022-06-04 22:07:06 +0530

    Update user group

--- client/src/pages/private/components/user-group.js
+++ client/src/pages/private/components/user-group.js
@@ -15,17 +15,17 @@ const UserGroup = ({group}) => {
     return (
         <Card sx={{minWidth: 275}}>
             <CardContent>
-                <Typography sx={{fontSize: 14}} color="text.secondary" gutterBottom>
+                <Typography sx={{fontSize: 14}} color="#800000" gutterBottom>
                     {group._id}
                 </Typography>
                 <Typography variant="h5" component="div">
                     {group.research_name}
                 </Typography>
-                <Typography variant="caption" color="text.secondary">
+                <Typography variant="caption" color="#800000">
                     Supervisor Approval : <span className={group.supervisor_approval === 'Pending'? 'text-warning':'text-success' }>{group.supervisor_approval}</span>
                 </Typography>
                 <br/>
-                <Typography variant="caption" color="text.secondary">
+                <Typography variant="caption" color="#800000">
                     Topic Approval : <span className={group.topic_approval === 'Pending' ? 'text-warning':'text-success' }>{group.topic_approval}</span>
                 </Typography>
             </CardContent>
